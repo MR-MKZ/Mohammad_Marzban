6ab9f90c0a30b6477680610a5458078b9e582a58
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2025-12-23 00:28:58 +0330

    Bump version from 0.8.15 to 0.8.17

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from fastapi.routing import APIRoute
 
 from config import ALLOWED_ORIGINS, DOCS, XRAY_SUBSCRIPTION_PATH
 
-__version__ = "0.8.15"
+__version__ = "0.8.17"
 
 app = FastAPI(
     title="MarzbanAPI",
